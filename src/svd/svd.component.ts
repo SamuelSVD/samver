169137ffe2eecdba0c97c1423910c35a04ade69f
SamuelSVD <svd_muelo1000@hotmail.com>
2020-03-16 00:44:00 -0400

    Last redirect to fix main not working

--- src/svd/svd.component.ts
+++ src/svd/svd.component.ts
@@ -8,9 +8,16 @@ import { Router } from '@angular/router';
 export class SvdComponent {
   title = 'samver';
   constructor(public _router:Router) {
-    console.log('Start');
-    console.log(sessionStorage.redirect.replace('https://samver.ca',''));
-    console.log('Continue');
-    this._router.navigate([sessionStorage.redirect.replace('https://samver.ca','')]);
+    var redirect = sessionStorage.redirect;
+    // Remove the URL from sessionStorage.
+    delete sessionStorage.redirect;
+    // Check if we actually need to redirect.
+    if (redirect && redirect != location.href) {
+      // We need to redirect to the URL the user was trying to access.
+      console.log('Start');
+      console.log(redirect.replace('https://samver.ca',''));
+      console.log('Continue');
+      this._router.navigate([redirect.replace('https://samver.ca','')]);
+    }
   }
 }
