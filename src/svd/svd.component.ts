66003ad9c3f1c3cbafcaabbf8ffe94f5777584c2
SamuelSVD <svd_muelo1000@hotmail.com>
2020-03-16 00:06:34 -0400

    Trying reroute again

--- src/svd/svd.component.ts
+++ src/svd/svd.component.ts
@@ -9,6 +9,9 @@ import { Location } from '@angular/common';
 export class SvdComponent {
   title = 'samver';
   constructor(public _router:Router, public _location: Location) {
+    console.log('Start');
+    console.log(this._location.path());
+    console.log('Continue');
     this._router.navigate([this._location.path()]);
   }
 }
