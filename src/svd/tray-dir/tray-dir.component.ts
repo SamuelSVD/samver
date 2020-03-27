485016ae0acaebba00894162de9c25a4b8a14695
SamuelSVD <svd_muelo1000@hotmail.com>
2020-03-26 23:59:08 -0400

    Making TrayDir load data from a json file rather than having to rewrite the form and rebuild the app

--- src/svd/tray-dir/tray-dir.component.ts
+++ src/svd/tray-dir/tray-dir.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as data from '../../assets/data/TrayDir.json';
 @Component({
   selector: 'app-tray-dir',
   templateUrl: './tray-dir.component.html',
   styleUrls: ['./tray-dir.component.css']
 })
 export class TrayDirComponent implements OnInit {
-
+  VersionInfo = (data as any).default;
   constructor() { }
 
   ngOnInit() {
