83fb29676137601e986855ba700ad4939390cf7b
SamuelSVD <svd.muelo1000+sourcetree@gmail.com>
2020-08-03 20:11:58 -0400

    updated home to get data from JSON file

--- src/svd/home/home.component.ts
+++ src/svd/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  lines = [];
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.httpClient.get("assets/data/Home.json").subscribe(data => {
+      console.log(data);
+      this.lines = (data as any).Lines;
+    })
   }
 
 }
