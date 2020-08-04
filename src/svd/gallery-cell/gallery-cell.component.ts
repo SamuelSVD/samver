16db8be8999ab8b2809667145ccdd44c9fddd211
SamuelSVD <svd.muelo1000+sourcetree@gmail.com>
2020-08-04 14:13:23 -0400

    Added baground colours while images load in the gallery

--- src/svd/gallery-cell/gallery-cell.component.ts
+++ src/svd/gallery-cell/gallery-cell.component.ts
@@ -12,9 +12,24 @@ export class GalleryCellComponent implements OnInit {
   @Input()
   public alt: string;
 
+  color: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public setStyle() {
+    console.log("is this running???")
+    this.color = '#';
+    var letters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    for (var i = 0; i < 6; i++) {
+        this.color += letters[Math.floor(Math.random() * 16)];
+    }
+    let styles = {
+      'background': this.color,
+      'background-image': 'radial-gradient(circle, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.25), rgba(255, 255, 255, 0.50))'
+    };
+    return styles;
+  }
 }
