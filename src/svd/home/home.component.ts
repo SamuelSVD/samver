e662d530d9c42a8e4a6773b1e8d5ed0d8922cf2f
SamuelSVD <svd_muelo1000@hotmail.com>
2020-09-25 00:34:48 -0400

    Worked on theme

--- src/svd/home/home.component.ts
+++ src/svd/home/home.component.ts
@@ -10,7 +10,40 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   lines = [];
   constructor(private httpClient: HttpClient, private styleService: StyleService) { }
-
+  themes = [
+    {
+      name: 'light',
+      styles: [
+        { selectorText: '.theme-color-bg', styleName: 'background', value: 'white' },
+        { selectorText: '.theme-color-bg', styleName: 'color', value: 'black' },
+        { selectorText: '.theme-color-box', styleName: 'background', value: '#5d6483' },
+        { selectorText: '.theme-color-box', styleName: 'color', value: 'white' },
+        { selectorText: '.theme-color-fill', styleName: 'fill', value: '#5d6483' },
+        { selectorText: '.theme-color-box-hover:hover', styleName: 'background', value: '#7D606E' },
+      ]
+    },
+    {
+      name: 'dark-green',
+      styles: [
+        { selectorText: '.theme-color-bg', styleName: 'background', value: 'rgb(15,107,184)' },
+        { selectorText: '.theme-color-bg', styleName: 'color', value: 'rgb(30,206,195)' },
+        { selectorText: '.theme-color-box', styleName: 'background', value: 'rgb(40,47,16)' },
+        { selectorText: '.theme-color-box', styleName: 'color', value: 'rgb(197,240,37)' },
+        { selectorText: '.theme-color-fill', styleName: 'fill', value: 'rgb(40,47,16)' },
+        { selectorText: '.theme-color-box-hover:hover', styleName: 'background', value: 'rgb(100,133,26)' },
+      ]
+    },
+/*    {
+      styles: [
+        { selectorText: '.theme-color-bg', styleName: 'background', value: '' },
+        { selectorText: '.theme-color-bg', styleName: 'color', value: '' },
+        { selectorText: '.theme-color-box', styleName: 'background', value: '' },
+        { selectorText: '.theme-color-box', styleName: 'color', value: '' },
+        { selectorText: '.theme-color-fill', styleName: 'fill', value: '' },
+        { selectorText: '.theme-color-box-hover:hover', styleName: 'background', value: '' },
+      ]
+    },*/
+  ];
   ngOnInit(): void {
     this.httpClient.get("assets/data/home.json").subscribe(data => {
       console.log(data);
@@ -18,7 +51,16 @@ export class HomeComponent implements OnInit {
     })
   }
   fun() {
-    console.log('lol');
+    this.randomTheme();
+  }
+  applyTheme(theme) {
+    console.log(theme.styles);
+    for(var i = 0; i < theme.styles.length; i++) {
+      var style = theme.styles[i];
+      this.styleService.setStyle(style.selectorText, style.styleName, style.value);
+    }
+  }
+  randomTheme() {
     this.styleService.setStyle(".theme-color-bg", "background", this.randomColorString());
     this.styleService.setStyle(".theme-color-bg", "color", this.randomColorString());
     var colour = this.randomColorString();
@@ -31,7 +73,7 @@ export class HomeComponent implements OnInit {
     var s = '#';
     for (var i = 0; i < 6; i++) {
       var j = Math.floor(Math.random() * Math.floor(16));
-      s+='0123456789abcdef'.charAt(j);
+      s += '0123456789abcdef'.charAt(j);
     }
     return s;
   }
